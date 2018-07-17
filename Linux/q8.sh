125de8f55b75ca4cc163bb88605a8d6f13b8096c
sagarmakwana9620 <40975890+sagarmakwana9620@users.noreply.github.com>
2018-07-17 13:21:54 +0530

    Update q8.sh

--- Linux/q8.sh
+++ Linux/q8.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Enter sides of triangle"
-read x1 x2 x3
-if [ $((x1+x2)) -lt $x3 ] || [ $((x2+x3)) -lt $x1 ] || [ $((x1+x3)) -lt $x2 ] ; then
+read x1 x2 x3 #taking 3 sides of triangle
+if [ $((x1+x2)) -lt $x3 ] || [ $((x2+x3)) -lt $x1 ] || [ $((x1+x3)) -lt $x2 ] ; then #checking if user has entered valid length
     echo "Enter Valid lengths"
 fi
 
