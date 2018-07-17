cb5df26931e04a268f7a273777107d7b1a7f3aaf
sagarmakwana9620 <40975890+sagarmakwana9620@users.noreply.github.com>
2018-07-17 13:17:04 +0530

    Update q5.sh

--- Linux/q5.sh
+++ Linux/q5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 crontab -e #enter the cron job file
-00 14 * * 1-5  /home/sagar/Desktop/file_1.sh 
-00 8-20/3 * * * /home/sagar/Desktop/file_1.sh
-00 00 1-31/2 2-12/2 * /home/sagar/Desktop/file_1.sh 
+00 14 * * 1-5  /home/sagar/Desktop/file_1.sh #execute a script on all weekdays at 2 PM.
+00 8-20/3 * * * /home/sagar/Desktop/file_1.sh #execute a script at every 3 hour interval between 8AM to 8PM
+00 00 1-31/2 2-12/2 * /home/sagar/Desktop/file_1.sh #execute a script on all the odd dates of all the even months at midnight
