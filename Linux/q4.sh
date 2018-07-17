18bb26aa1dd12e01e1a807bd96c61d8b861a7be1
sagarmakwana9620 <40975890+sagarmakwana9620@users.noreply.github.com>
2018-07-17 13:15:30 +0530

    Update q4.sh

--- Linux/q4.sh
+++ Linux/q4.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "What is your name?"
-read name
+read name #taking input from user using read function
 echo "I will create a file named ${name}_file"
-touch ${name}_file
+touch ${name}_file #touch is used to create file
 echo "Enter a sentence to write in the file"
-read line
-echo $line > ${name}_file
+read line #taking input to be written to file
+echo $line > ${name}_file  #storing input in file
