93dc9f253a387d995607a9e90e26d97dc240596a
sagarmakwana9620 <40975890+sagarmakwana9620@users.noreply.github.com>
2018-07-17 13:13:15 +0530

    Update q3.sh

--- Linux/q3.sh
+++ Linux/q3.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "Current date and time is $(date)" 
-echo "User is $(whoami)" 
-echo "Current working directory $(pwd)"
-# echo "Current date and time is `pwd`"	
+echo "Current date and time is $(date)"  # date function is used to display current date and time   
+echo "User is $(whoami)" #whoami function is used to display current user
+echo "Current working directory $(pwd)" #pwd function is used to display present working directory
